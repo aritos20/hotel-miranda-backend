c5bb51d7a1759ef5ef836e858211d5c3a4e88ce0
aritos20 <agianicopulos@gmail.com>
2023-03-24 11:27:04 +0100

    changed to async/await instead of then/catch in bookings controllers

--- src/controllers/bookings.ts
+++ src/controllers/bookings.ts
@@ -3,53 +3,53 @@ import { dbQuery } from '../database/connection';
 import { Bookings } from "../interfaces/fakerBooking.interface";
 import { handleHttp } from "../utils/error.handle";
 
-const getBooking = (req: Request, res: Response, next: NextFunction) => {
+const getBooking = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        dbQuery('SELECT * FROM bookings WHERE booking_id = ?;', req.params.bookingid)
-            .then((booking: Bookings | unknown) => res.json(booking))
-            .catch((e: Error): void => next(e));
+        const booking: Bookings | unknown = await dbQuery('SELECT * FROM bookings WHERE booking_id = ?;', req.params.bookingid);
+        res.json(booking);
     } catch(e) {
-       handleHttp(res, 'ERROR_GET_BOOKING');
+        next(e);
+        handleHttp(res, 'ERROR_GET_BOOKING');
     }
 }
 
-const getBookings = (req: Request, res: Response, next: NextFunction) => {
+const getBookings = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        dbQuery('SELECT * FROM bookings;', null)
-            .then((bookings: Bookings | unknown) => res.json(bookings))
-            .catch((e: Error) => next(e));
+        const bookings: Bookings | unknown = await dbQuery('SELECT * FROM bookings;', null);
+        res.json(bookings);
     } catch(e) {
-       handleHttp(res, 'ERROR_GET_BOOKINGS');
+        next(e);
+        handleHttp(res, 'ERROR_GET_BOOKINGS');
     }
 }
 
-const updateBooking = (req: Request, res: Response, next: NextFunction) => {
+const updateBooking = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        dbQuery('UPDATE bookings SET ? WHERE booking_id = ?;', [req.body, req.params.bookingid])
-            .then((booking: Bookings | unknown) => res.json({message: 'booking update succesfully', booking: req.body}))
-            .catch((e: Error) => next(e))
+        await dbQuery('UPDATE bookings SET ? WHERE booking_id = ?;', [req.body, req.params.bookingid]);
+        res.json({success: true, message: 'booking update succesfully', booking: req.body});
     } catch(e) {
-       handleHttp(res, 'ERROR_UPDATE_BOOKING');
+        next(e);
+        handleHttp(res, 'ERROR_UPDATE_BOOKING');
     }
 }
 
-const postBooking = (req: Request, res: Response, next: NextFunction) => {
+const postBooking = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        dbQuery('INSERT INTO bookings SET ?', req.body)
-            .then((booking: Bookings | unknown) => res.json({message: 'booking succesfully done', booking: req.body}))
-            .catch((e: Error) => next(e));
+        await dbQuery('INSERT INTO bookings SET ?', req.body);
+        res.json({success: true, message: 'booking succesfully done', booking: req.body});
     } catch(e) {
-       handleHttp(res, 'ERROR_POST_BOOKING');
+        next(e);
+        handleHttp(res, 'ERROR_POST_BOOKING');
     }
 }
 
-const deleteBooking = (req: Request, res: Response, next: NextFunction) => {
+const deleteBooking = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        dbQuery('DELETE FROM bookings WHERE booking_id = ?;', req.params.bookingid)
-            .then(() => res.json({message: 'booking deleted'}))
-            .catch((e: Error) => next(e))
+        await dbQuery('DELETE FROM bookings WHERE booking_id = ?;', req.params.bookingid)
+        res.json({success: true, message: 'booking deleted'});
     } catch(e) {
-       handleHttp(res, 'ERROR_DELETE_BOOKING');
+        next(e);
+        handleHttp(res, 'ERROR_DELETE_BOOKING');
     }
 }
 
